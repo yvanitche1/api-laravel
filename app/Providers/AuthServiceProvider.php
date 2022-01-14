b52d7dcdae88906a6f90fa8b77fc1c3800b2d5a8
DJILA YVAN <yvanitchedjila@gmail.com>
2022-01-14 15:21:22 +0100

    initial

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Providers;
+
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    /**
+     * The policy mappings for the application.
+     *
+     * @var array<class-string, class-string>
+     */
+    protected $policies = [
+        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+    ];
+
+    /**
+     * Register any authentication / authorization services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerPolicies();
+
+        Passport::routes();
+
+        //Client ID: 1
+        //Client secret: d3RIIzkJSYOSfGQuIXrcYOkW3W3gD9IUwSTtf0GH
+
+        
+    }
+}
